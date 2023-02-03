8a42f7e92f140ee5bcfd629fa7f04b7bff840b4c
aorigemst <tarsismsm@gmail.com>
2023-02-03 14:48:09 -0300

    criando uma janela

--- jornada_dev/bootcamp_dio_backend/projetos_java/java_anatomia_de_classes/anatomia_de_classes/src/MyClass.java
+++ jornada_dev/bootcamp_dio_backend/projetos_java/java_anatomia_de_classes/anatomia_de_classes/src/MyClass.java
@@ -16,25 +16,9 @@ public class MyClass extends JFrame {
                 
             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-                
-        
-        System.out.println ("Explicando esse método : main");
+             
+              
 
-        
-
-        System.out.println ("Se a Classe é uma classe executável, realizando uma inicialização dentro do projeto, pro exemplo, ela precisa de um método especial, um método principal, método `main`. sendo criado como : `public static void main (String[] args)`");
-
-        
-
-        System.out.println ("void -> apenas executa sem retornar nada; espera um parâmetro especial do tipo String; parâmetro de array args; corpo representado por {}");
-
-        
-
-        System.out.println ("Em todas as classes, as primeiras letras de cada palava e subpalavra deverão ser maúsculas: Sample - MyClass.java");
-
-      
-
-        System.out.println ("Toda Classe precisa existir dentro do diretório src do projeto");
     }
     
 }
